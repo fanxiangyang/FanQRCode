0bd4a583ec5af3a85be5bd097a46999b3d7d3ef3
fanxiangyang <1346569137@qq.com>
2015-05-07 08:28:30 +0800

    Initial Commit

---
+++ QRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCode
+//
+//  Created by Fan on 15/5/7.
+//  Copyright (c) 2015年 未名空间. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCode
+//
+//  Created by Fan on 15/5/7.
+//  Copyright (c) 2015年 未名空间. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
